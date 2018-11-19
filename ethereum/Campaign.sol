0db09d5635b79fb2e27c392afa2db295eac1772d
daniellara <daniel.lara.diezma@gmail.com>
2018-11-19 21:38:53 +0100

    Add contribute function

--- ethereum/Campaign.sol
+++ ethereum/Campaign.sol
@@ -4,9 +4,19 @@ contract Campaign {
 
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
 
     constructor (uint _minAmount) public {
         manager = msg.sender;
         minimumContribution = _minAmount;
+    }
+
+    function contribute() public payable{
+        require(
+            msg.value >= minimumContribution,
+            "Please insert at least the minimum contribution"
+        );
+
+        approvers.push(msg.sender);
     }
 }
