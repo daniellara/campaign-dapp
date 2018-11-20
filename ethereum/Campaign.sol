88d9168de9b7e92340563db7fa99e6c8fe795051
daniellara <daniel.lara.diezma@gmail.com>
2018-11-20 17:35:15 +0100

    Add createRequest method

--- ethereum/Campaign.sol
+++ ethereum/Campaign.sol
@@ -2,9 +2,25 @@ pragma solidity ^0.4.24;
 
 contract Campaign {
 
+    struct Request {
+        string description;
+        uint amount;
+        address vendor;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
+
+    modifier restricted() {
+        require(
+            msg.sender == manager,
+            "Only the manager can call this function"
+        );
+        _;
+    }
 
     constructor (uint _minAmount) public {
         manager = msg.sender;
@@ -19,4 +35,16 @@ contract Campaign {
 
         approvers.push(msg.sender);
     }
+
+    function createRequest(string _description, uint _amount, address _vendor) public restricted{
+        Request req = Request({
+            description: _description,
+            amount: _amount,
+            vendor: _vendor,
+            complete: false
+        });
+
+        requests.push(req);
+    }
+
 }
